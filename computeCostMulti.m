a1ad17abd9eb2d1daec0566dab26c1cc0c4d548d
Michael <wzk0731@gmail.com>
2015-09-20 18:04:56 +0800

    add

---
+++ computeCostMulti.m
@@ -0,0 +1,33 @@
+function J = computeCostMulti(X, y, theta)
+%COMPUTECOSTMULTI Compute cost for linear regression with multiple variables
+%   J = COMPUTECOSTMULTI(X, y, theta) computes the cost of using theta as the
+%   parameter for linear regression to fit the data points in X and y
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta
+%               You should set J to the cost.
+
+h=X*theta; % h is a m*1 vector that stores the each h(xi)
+sum=0;  %sum of the sigma part before dividing 2m
+for i=1:m
+    temp=0;
+    temp=h(i)-y(i);
+    temp=temp*temp;
+    sum=sum+temp;
+end
+
+sum=sum/m;
+
+J=sum/2;
+
+
+
+% =========================================================================
+
+end
---
+++ gradientDescentMulti.m
@@ -0,0 +1,48 @@
+function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENTMULTI Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENTMULTI(x, y, theta, alpha, num_iters) updates theta by
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCostMulti) and gradient here.
+    %
+
+
+    h=X*theta; % h(xi)  a m*1 vector
+    sum=zeros(size(X,2),1);
+    
+    for i=1:m
+        for j=1:size(X,2)
+            sum(j)=sum(j)+(h(i)-y(i))*X(i,j);
+        end
+        
+    end
+
+    for j=1:size(X,2)
+        sum(j)=sum(j)*alpha/m;
+    end
+    
+    for j=1:size(X,2)
+        theta(j)=theta(j)-sum(j);
+    end
+
+    
+
+    % ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCostMulti(X, y, theta);
+
+end
+
+end
